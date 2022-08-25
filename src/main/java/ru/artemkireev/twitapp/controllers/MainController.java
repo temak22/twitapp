d5db7430fab58d295d893cfdcaab9a448be00c91
temak22 <kireev.a.o@edu.mirea.ru>
2022-08-25 15:18:16 +0300

    Add file upload and tune access to static files

--- src/main/java/ru/artemkireev/twitapp/controllers/MainController.java
+++ src/main/java/ru/artemkireev/twitapp/controllers/MainController.java
@@ -1,6 +1,7 @@
 package ru.artemkireev.twitapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,15 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import ru.artemkireev.twitapp.entities.Message;
 import ru.artemkireev.twitapp.repositories.MessageRepo;
 import ru.artemkireev.twitapp.security.entities.User;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 @RequestMapping
 public class MainController {
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @Autowired
     private MessageRepo messageRepo;
@@ -45,8 +53,25 @@ public class MainController {
     public String addMessage(@AuthenticationPrincipal User user,
                              @RequestParam String text,
                              @RequestParam String tag,
-                             Map<String, Object> model) {
+                             @RequestParam("file") MultipartFile file,
+                             Map<String, Object> model) throws IOException {
         Message message = new Message(text, tag, user);
+
+        if (file != null && !file.isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists())
+                uploadDir.mkdir();
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            message.setFilename(resultFilename);
+        }
+
+
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
--- src/main/java/ru/artemkireev/twitapp/entities/Message.java
+++ src/main/java/ru/artemkireev/twitapp/entities/Message.java
@@ -16,12 +16,14 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
+    private String text;
+    private String tag;
+
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User author;
 
-    private String text;
-    private String tag;
+    private String filename;
 
     public Message(String text, String tag, User user) {
         this.text = text;
